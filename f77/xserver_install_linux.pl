54bfe561eeacf30fe69183ba3d1803f9dd2ce8b9
sergeys21 <81265567+sergeys21@users.noreply.github.com>
2026-04-23 10:06:06 -0500

    Improved binary search in the installation script

--- f77/xserver_install_linux.pl
+++ f77/xserver_install_linux.pl
@@ -10,22 +10,28 @@
   select STDERR; $|=1;				# Set unbuffered output
   select STDOUT; $|=1;				# Set unbuffered output
 
-  my ($dest_x0h, $dirs_prefix, $dirs_postfix);
-  my (@dirs_x0h, $ndirs, $i, @exefiles, $exefile, $exe, $script);
+  my ($dest_x0h, $dirs_prefix, $dirs_postfix, $codeblocks, @dirs);
+  my (@dirs_x0h, $ndirs, $i, @exefiles, $exefile, $script);
 
   print STDOUT 'This script installs X-server executables for linux'."\n";
 
+  $codeblocks = 'CodeBlocks';
   if (defined $ENV{X0H}) {
      $dest_x0h = $ENV{X0H};
      $script = abs_path($0);
      $dirs_prefix = dirname($script);
+     @dirs = split(/\n/,`find $dirs_prefix -name xserver.workspace`);
+     if ($dirs[0] !~ $codeblocks) {die '*** Cannot find '.$codeblocks.' folder under '.$dirs_prefix."\n";}
+     $dirs[0] =~ s/${codeblocks}.*$/$codeblocks/;
+     $dirs_prefix = $dirs[0];
+     print STDOUT 'dir="'.$dirs_prefix.'"'."\n";
   } else {
-     die 'Please define X0H environment to point it to the x0h directory.'."\n";
+     die '*** Please define X0H environment pointing to the x0h folder.'."\n";
   }
   print STDOUT '       X0H directory: '.$dest_x0h."\n";
   print STDOUT 'Codeblocks directory: '.$dirs_prefix."\n";
-  if (! -e $dest_x0h)    {die '*** X0H directory "'.$dest_x0h.'" does not exist.';}
-  if (! -e $dirs_prefix) {die '*** Codeblocks directory "'.$dirs_prefix.'" does not exist.';}
+  if (! -e $dest_x0h)    {die '*** X0H folder "'.$dest_x0h.'" does not exist.';}
+  if (! -e $dirs_prefix) {die '*** Codeblocks folder "'.$dirs_prefix.'" does not exist.';}
 
   @dirs_x0h = qw(
 brl_build
@@ -50,7 +56,10 @@ x0p_web
      $dirs_x0h[$i] = $dirs_prefix.'/'.$dirs_x0h[$i].'/bin/';
      if    (-e $dirs_x0h[$i].'Release') {$dirs_postfix = 'Release';}
      elsif (-e $dirs_x0h[$i].'Debug')   {$dirs_postfix = 'Debug';}
-     else                               {undef $dirs_postfix;}
+     else {
+        print STDOUT '!!! Neither '.$dirs_x0h[$i].'Release nor '.$dirs_x0h[$i].'Debug found!'."\n";
+        undef $dirs_postfix;
+     }
      if (defined $dirs_postfix) {
         $dirs_x0h[$i] .= $dirs_postfix;
         @exefiles = glob($dirs_x0h[$i].'/*');
